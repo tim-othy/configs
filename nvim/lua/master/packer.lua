22cae9d9cac76f93ae933e814d6ec7b0c71a64f2
tim-othy <timothy.mehay@gmail.com>
2023-05-21 21:42:17 +0200

    Add neo-tree to nvim config

--- nvim/lua/master/packer.lua
+++ nvim/lua/master/packer.lua
@@ -43,4 +43,16 @@ return require('packer').startup(function(use)
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 
+    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = { 
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        }
+    }
+
 end)
